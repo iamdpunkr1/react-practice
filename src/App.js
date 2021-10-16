56d84a7dd08a5800954a9e62ef34af35acace604
Dipankar Prasad <dipankar.prasad@timesinternet.in>
2021-10-16 15:00:11 +0530

    project finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import Todo from './components/Todo';
+import { Route, Switch } from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites';
+import Layout from './components/layout/Layout';
 
 function App() {
-  return <div>
-            <h1>My Todos</h1>
-            <Todo title='Learn React'/>
-            <Todo title='Learn RTL'/>
-            <Todo title='Master Jest & RTL'/>
-          </div>
+  return (
+    <Layout>
+      <Switch>
+        <Route path='/' exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path='/new-meetup'>
+          <NewMeetupPage />
+        </Route>
+        <Route path='/favorites'>
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </Layout>
+  );
 }
 
 export default App;
